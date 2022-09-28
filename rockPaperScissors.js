16c01b7e0b99df418bef6e7da600478fc2025dd4
Niclas Geuken <niclas.fg@gmail.com>
2022-09-28 21:20:09 +0200

    add playerchoice, computerChoice and comparison

---
+++ rockPaperScissors.js
@@ -0,0 +1,43 @@
+console.log("Lets play Rock Paper scissors!");
+
+const results = ["Rock", "Paper", "Scissors"];
+const playerChoiceText = "Choose your weapon: \n 1: Rock \n 2: Paper \n 3: Scissors\n";
+
+function getComputerChoice() {
+    var computerChoice = results[Math.floor(Math.random() * 3)];
+    console.log("The computer choose: \t" + computerChoice);
+    return computerChoice; 
+}
+
+function getPlayerChoice(){
+    while (true) {       
+        var playerChoice = prompt(playerChoiceText);
+        switch (playerChoice) {
+            case "1":
+            case "2":
+            case "3":
+                console.log("The player choose: \t" + results[playerChoice - 1]);
+                return results[playerChoice - 1];  
+        }
+    }
+}
+
+function playRound(){
+    var playerChoice = results.indexOf(getPlayerChoice());
+    var computerChoice = results.indexOf(getComputerChoice());
+
+    var comparison = ((playerChoice - computerChoice)%3 + 3) %3;
+
+    if (comparison == 0) {
+        console.log("It's a tie!");
+    }
+    else if (comparison == 1) {
+        console.log("You win!");
+    }
+    else {
+        console.log("You lose...");
+    }
+
+}
+
+playRound()
